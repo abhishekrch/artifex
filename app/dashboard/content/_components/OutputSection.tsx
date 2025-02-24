ae38d0652b622d9e42edbec829f520cf57605a2e
Abhishek Choudhary <pmoabhishek@gmail.com>
2025-02-24 23:11:59 +0530

    feat(ui): create OutputSection with rich text editor

--- app/dashboard/content/_components/OutputSection.tsx
+++ app/dashboard/content/_components/OutputSection.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import { useRef } from "react";
+import "@toast-ui/editor/dist/toastui-editor.css";
+import { Editor } from "@toast-ui/react-editor";
+import { Button } from "@/components/ui/button";
+import { Copy } from "lucide-react";
 
 function OutputSection() {
+  const editorRef:any = useRef(null);
+
+  const handleEditorChange = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getInstance().getMarkdown());
+    }
+  };
+
   return (
-    <div>
-      OutputSection
+    <div className="bg-white shadow-lg border rounded-lg">
+      <div className="flex justify-between items-center p-5">
+        <h2 className="font-medium text-lg">Your Result</h2>
+        <Button className="flex gap-2">
+          <Copy className="w-4 h-4" /> Copy
+        </Button>
+      </div>
+      <Editor
+      ref={editorRef}
+        initialValue="Your result will appear here"
+        initialEditType="wysiwyg"
+        height="600px"
+        useCommandShortcut={true}
+        onChange={handleEditorChange}
+      />
     </div>
-  )
+  );
 }
 
-export default OutputSection
+export default OutputSection;
