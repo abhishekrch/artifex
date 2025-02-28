9104af6120d9a299ccb9f0eb54a9a344f9b5e1ed
Abhishek Choudhary <pmoabhishek@gmail.com>
2025-02-28 15:45:35 +0530

    chore(db): set up Drizzle ORM and define database schema

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  out: './drizzle',
+  schema: './utils/schema.tsx',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL!,
+  },
+});
